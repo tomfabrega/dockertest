a415c4521a5ef35b92065216192b70cf25fd4a57
KuKIGG <niklasgerwens@hotmail.de>
2022-09-10 20:25:13 +0200

    Updated maven jar

--- src/main/java/TestPrinter.java
+++ src/main/java/TestPrinter.java
@@ -1,5 +1,13 @@
+import java.io.Console;
+import java.io.IOException;
+
 public class TestPrinter {
     public static void main(String[] args) {
-        System.out.println("Test Nachricht generiert in Java TestPrinter Class. Neue Nachricht wenn Tom keine kacke gebaut hat!");
+        System.out.println("Test Nachricht generiert in Java TestPrinter Class. Neue Nachricht wenn Tom keine kacke gebaut hat! User Input beendet den Service");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
