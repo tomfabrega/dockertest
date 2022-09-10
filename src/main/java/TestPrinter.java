18ea98333c448b7f8afad6ebf93d184c23a5fae7
KuKIGG <niklasgerwens@hotmail.de>
2022-09-10 20:19:45 +0200

    Updated maven jar

--- src/main/java/TestPrinter.java
+++ src/main/java/TestPrinter.java
@@ -1,5 +1,5 @@
 public class TestPrinter {
     public static void main(String[] args) {
-        System.out.println("Test Nachricht generiert in Java TestPrinter Class");
+        System.out.println("Test Nachricht generiert in Java TestPrinter Class. Neue Nachricht wenn Tom keine kacke gebaut hat!");
     }
 }
