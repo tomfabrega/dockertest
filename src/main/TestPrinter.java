415c42b48938ac7e29470953c7a5a2077b433ce0
KuKIGG <niklasgerwens@hotmail.de>
2022-09-10 16:47:22 +0200

    Test Print added

---
+++ src/main/TestPrinter.java
@@ -0,0 +1,7 @@
+package main;
+
+public class TestPrinter {
+    public static void main(String[] args) {
+        System.out.println("Test Nachricht generiert in Java TestPrinter Class");
+    }
+}
